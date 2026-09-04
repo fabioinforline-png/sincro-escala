1b7e28ba52dc34c3ce9aaa94a3b63d63c76ab702
fabioinforline-png <fabioinforline@gmail.com>
2026-09-04 19:52:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from pathlib import Path
+from funcionario import cadastra_funcionario,listar_funcionario,excluir_funcionario
+from escala import cadastrar_escala
 
-path_bd=Path("sincro_escala/BD") / "funcionario_bd.txt"
-funcionarios=[]
+
 sair_do_sistema=False
 
 def apresenta_menu():
@@ -12,46 +12,12 @@ def apresenta_menu():
     print("1. Cadastrar funcionário👤")
     print("2. Listar funcionário 👥")
     print("3. Excluir funcionário 🗑️")
+    print("4. Cadastrar Escala 👥📅")
     print("0. Sair ❌")
 
     print("")
     opçao_menu=input("Escolha uma opção: ")
     return opçao_menu
-
-def cadastra_funcionario():  
-    funcionario=input("digite o nome do funcionario: ")
-    with open(path_bd, "a", encoding="utf-8") as arquivo:
-        arquivo.write(f"{funcionario}\n")
-    funcionarios.append(funcionario)
-    print(f"o nome cadastrado foi: {funcionario} ")
-    print("=========================================")
-    print("Você gostaria de adiocionar um novo funcionario?")
-    print("1. sim✅ ")
-    print("2. não❌")
-    seguir_cadastro=input("Escolha uma opção: ")
-    print("=========================================")
-    if seguir_cadastro=="1":
-        cadastra_funcionario()
-    if seguir_cadastro=="2":
-        print("cadastro concluido")
-
-def listar_funcionario():
-    with open(path_bd, "r",encoding="utf-8") as arquivo:
-        for linha in arquivo:
-            print(linha.strip())
- 
-def excluir_funcionario():
-    listar_funcionario()
-    funcionario=input("Qual funcionário você deseja deletar: ")
-    with open(path_bd, "r",encoding="utf-8") as arquivo:
-        nomes=arquivo.readline()
-    
-    
-    with open(path_bd, "w",encoding="utf-8") as arquivo:
-        for linha in nomes:
-            if linha.strip()==funcionario:
-                linha=""
-            arquivo.write(linha)
         
 def Sair():
     print("saindo do sistema de gestão de escala 👋")    
@@ -67,6 +33,8 @@ while not sair_do_sistema:
             listar_funcionario()
         case "3":
             excluir_funcionario()
+        case "4":
+            cadastrar_escala()
         case "0":
             Sair()
             break
